a8f456f4fb88be175f90279d5e98eb847a20f795
motty mandel <mandelmottyisrael65@gmail.com>
2024-01-16 15:26:42 -0500

    cleaned it up a little

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import './footer.css';
 export default function Footer() {
     return (
         <div className='footer'>
-            <h4>&copy; Ocean Auto Glass </h4>
+            <h5>&copy; Ocean Auto Glass </h5>
             <a href="https://www.google.com/maps/place/Ocean+Auto+Glass/@40.0565704,-74.2292737,17z/data=!4m14!1m7!3m6!1s0x89c1831fe4f2c891:0x32bb8d88da005120!2sOcean+Auto+Glass!8m2!3d40.0565704!4d-74.2266934!16s%2Fg%2F11h9hvt428!3m5!1s0x89c1831fe4f2c891:0x32bb8d88da005120!8m2!3d40.0565704!4d-74.2266934!16s%2Fg%2F11h9hvt428?entry=ttu" target='_blank'>Google Maps</a>
         </div>
     );
--- client/src/pages/Static.jsx
+++ client/src/pages/Static.jsx
@@ -3,18 +3,27 @@ import './static.css';
 export default function Static() {
     return (
         <div className='main'>
+
             <div className="static1">
                 <div className="box1">
-                    <h3>Done at your home or at work - wherever you are!</h3>
-                </div>
-                <div className="image">
-                    <img src="google-review.png" alt="google-review" />
+                    <h3>Done at your location or at our shop - whichever suits you better!</h3>
                 </div>
             </div>
+
             <div className="static2">
-            <div className='review1'>
+                <div className="reviews">
+                    <img src="google-review.png" alt="google-review" />
+                </div>
+                <div className='review1'>
                     <img src="review-1.png" alt="review1" />
                 </div>
+                <div className="review-2">
+                    <img src="review-2.png" alt="motty" />
+                </div>
+            </div>
+
+
+            <div className="static3">
                 <div className="motty">
                     <img src="motty-at-work.jpg" alt="motty" />
                 </div>
