334823b8292dd5bc0fbbf355058e53a28ed9e520
motty mandel <mandelmottyisrael65@gmail.com>
2024-01-16 15:38:56 -0500

    cleaned it up a little

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import '../index.css'
 import './header.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -6,11 +8,11 @@ export default function Header() {
         <div className='header'>
             <h1>Ocean Auto Glass</h1>
 
-            <div className='buttons'>
+
                 <h5>Call now at 732-677-8472</h5>
                 {/* <button id='signUpBtn'>Sign Up</button>
                 <button id='loginBtn'>Login</button> */}
-            </div>
+
 
         </div>
     );
